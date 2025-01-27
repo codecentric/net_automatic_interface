73bb07b224b2048fa0eeb30d38491d31a76abd75
Christian Sauer <christian.sauer@codecentric.de>
2025-01-27 08:41:57 +0100

    feat: Reorganize tests to use Verify. Drop FluentAssertions due to license change. Reorganize Tests

---
+++ AutomaticInterface/Tests/Infrastructure.cs
@@ -0,0 +1,50 @@
+﻿using AutomaticInterface;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Tests;
+
+public static class Infrastructure
+{
+    public static string GenerateCode(string code)
+    {
+        var syntaxTree = CSharpSyntaxTree.ParseText(code);
+        var references = AppDomain
+            .CurrentDomain.GetAssemblies()
+            .Where(assembly => !assembly.IsDynamic)
+            .Select(assembly => MetadataReference.CreateFromFile(assembly.Location))
+            .Cast<MetadataReference>();
+
+        var compilation = CSharpCompilation.Create(
+            "SourceGeneratorTests",
+            [syntaxTree],
+            references,
+            new(OutputKind.DynamicallyLinkedLibrary)
+        );
+
+        // defensive check to avoid errors in the source text, which can happen with manually written code
+        var sourceDiagnostics = compilation.GetDiagnostics();
+        var sourceErrors = sourceDiagnostics
+            .Where(d => d.Severity == DiagnosticSeverity.Error)
+            .Where(x => x.Id != "CS0246") // missing references are ok
+            .ToList();
+
+        Assert.Empty(sourceErrors);
+
+        var generator = new AutomaticInterfaceGenerator();
+
+        CSharpGeneratorDriver
+            .Create(generator)
+            .RunGeneratorsAndUpdateCompilation(
+                compilation,
+                out var outputCompilation,
+                out var diagnostics
+            );
+
+        var errors = diagnostics.Where(d => d.Severity == DiagnosticSeverity.Error).ToList();
+
+        Assert.Empty(errors);
+
+        return outputCompilation.SyntaxTrees.Skip(1).LastOrDefault()?.ToString();
+    }
+}
---
+++ AutomaticInterface/Tests/TypeResolutions/TypeResolutions.cs
@@ -0,0 +1,173 @@
+﻿namespace Tests.TypeResolutions;
+
+public class TypeResolutions
+{
+    [Fact]
+    public async Task WorksWithFileScopedNamespace()
+    {
+        const string code = """
+
+            using AutomaticInterface;
+
+            namespace AutomaticInterfaceExample;
+
+            [GenerateAutomaticInterface]
+            class DemoClass
+            {
+                public string Hello { get; set; }
+            }
+            """;
+
+        await Verify(Infrastructure.GenerateCode(code));
+    }
+
+    [Fact]
+    public async Task WorksWithBracedNamespace()
+    {
+        const string code = """
+
+            using AutomaticInterface;
+
+            namespace AutomaticInterfaceExample
+            {
+
+                [GenerateAutomaticInterface]
+                class DemoClass
+                {
+                    public string Hello { get; set; }
+                }
+            }
+            """;
+
+        await Verify(Infrastructure.GenerateCode(code));
+    }
+
+    [Fact]
+    public async Task WorksWithUsingAliases()
+    {
+        const string code = """
+
+            using AutomaticInterface;
+            using TaskAlias = System.Threading.Tasks.Task;
+
+            namespace AutomaticInterfaceExample;
+
+            [GenerateAutomaticInterface]
+            class DemoClass
+            {
+                public TaskAlias Hello { get; set; }
+            }
+
+
+            """;
+
+        await Verify(Infrastructure.GenerateCode(code));
+    }
+
+    [Fact]
+    public async Task WorksWithIdenticalTypeNames()
+    {
+        const string code = """
+
+            using AutomaticInterface;
+            namespace AutomaticInterfaceExample
+            {
+                namespace Models1 {
+                    public class Model;
+                }
+
+                namespace Models2 {
+                    public class Model;
+                }
+
+                [GenerateAutomaticInterface]
+                public class ModelManager
+                {
+                    public Models1.Model GetModel1() => null!;
+                    public Models2.Model GetModel2() => null!;
+                }
+            }
+            """;
+
+        await Verify(Infrastructure.GenerateCode(code));
+    }
+
+    [Fact]
+    public async Task WorksWithNestedUsings()
+    {
+        const string code = """
+
+            using AutomaticInterface;
+            namespace RootNamespace
+            {
+                namespace Models
+                {
+                    public class Model;
+                }
+
+                namespace ModelManager
+                {
+                    using Models;
+
+                    [GenerateAutomaticInterface]
+                    public class ModelManager
+                    {
+                        public Model GetModel() => null!;
+                    }
+                }
+            }
+            """;
+
+        await Verify(Infrastructure.GenerateCode(code));
+    }
+
+    [Fact]
+    public async Task WorksWithShadowedGlobalNamespace()
+    {
+        const string code = """
+            using AutomaticInterface;
+            using Task = System.Threading.Tasks.Task;
+
+            namespace AutomaticInterfaceExample
+            {
+                namespace System.Threading.Tasks
+                {
+                    public class Task;
+                }
+
+                [GenerateAutomaticInterface]
+                public class DemoClass
+                {
+                    public Task GetTask() => null!;
+                }
+            }
+            """;
+
+        await Verify(Infrastructure.GenerateCode(code));
+    }
+
+    [Fact]
+    public async Task WorksWithGlobalUsing()
+    {
+        const string code = """
+            global using GlobalNamespace;
+            using AutomaticInterface;
+
+            namespace GlobalNamespace
+            {
+                public class AClass;
+            }
+
+            namespace AutomaticInterfaceExample
+            {
+                [GenerateAutomaticInterface]
+                public class DemoClass
+                {
+                    public AClass GetClass() => null!;
+                }
+            }
+            """;
+
+        await Verify(Infrastructure.GenerateCode(code));
+    }
+}
