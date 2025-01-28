6f7f480eda599980156ed94cb287f16a90a49eeb
Christian Sauer <christian.sauer@codecentric.de>
2025-01-28 09:00:32 +0100

    feat: tests using verify

---
+++ AutomaticInterface/Tests/Enums/Enums.cs
@@ -0,0 +1,73 @@
+﻿namespace Tests.Enums;
+
+public class Enums
+{
+    [Fact]
+    public async Task WorksWithByteEnums()
+    {
+        const string code = """
+
+            using AutomaticInterface;
+            using System;
+
+            namespace AutomaticInterfaceExample;
+
+            public enum EnumWithByteType : byte { A = 1, B = 2, C = 3 };
+
+            [GenerateAutomaticInterface]
+            public class DemoClass
+            {
+                public void MethodWithDefaultParameter(EnumWithByteType a = EnumWithByteType.B) { }
+            }
+
+            """;
+
+        await Verify(Infrastructure.GenerateCode(code));
+    }
+
+    [Fact]
+    public async Task WorksWithByteEnumsAsReturnType()
+    {
+        const string code = """
+
+            using AutomaticInterface;
+            using System;
+
+            namespace AutomaticInterfaceExample;
+
+            public enum EnumWithByteType : byte { A = 1, B = 2, C = 3 };
+
+            [GenerateAutomaticInterface]
+            public class DemoClass
+            {
+                public EnumWithByteType MethodWithDefaultParameter(EnumWithByteType a = EnumWithByteType.B) { return a; }
+            }
+
+            """;
+
+        await Verify(Infrastructure.GenerateCode(code));
+    }
+
+    [Fact]
+    public async Task WorksWithByteEnumsProperties()
+    {
+        const string code = """
+
+            using AutomaticInterface;
+            using System;
+
+            namespace AutomaticInterfaceExample;
+
+            public enum EnumWithByteType : byte { A = 1, B = 2, C = 3 };
+
+            [GenerateAutomaticInterface]
+            public class DemoClass
+            {
+               public EnumWithByteType SomeProperty { get; set; }  
+            }
+
+            """;
+
+        await Verify(Infrastructure.GenerateCode(code));
+    }
+}
