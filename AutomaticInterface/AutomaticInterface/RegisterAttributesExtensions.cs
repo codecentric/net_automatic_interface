28d6bc9a43a330e96b70231875edcd2e55dcaca5
Ihor Kuzmychov <i.kuzmychov@spd.tech>
2025-11-13 23:41:14 +0100

    Update package version and attribute summary

--- AutomaticInterface/AutomaticInterface/RegisterAttributesExtensions.cs
+++ AutomaticInterface/AutomaticInterface/RegisterAttributesExtensions.cs
@@ -32,7 +32,7 @@ internal static class RegisterAttributesExtensions
                             /// <summary>
                             /// Use source generator to automatically create a Interface from this class
                             /// </summary>
-                            /// <param name="namespaceName">Namespace name for the generated interface. Defaults to the same namespace as the class.</param>
+                            /// <param name="namespaceName">Namespace name for the generated interface. Defaults to the same namespace as the class. The '*' character may be used as a placeholder for the containing type's namespace, allowing you to specify only a prefix for the target namespace instead of its full name.</param>
                             /// <param name="interfaceName">Interface name for the generated interface. Defaults to an interface version of the class name, e.g ExampleClass -> IExampleClass.</param>
                             /// <param name="asInternal">If true, the generated interface will be internal, otherwise public</param>
                             internal {{AutomaticInterfaceGenerator.DefaultAttributeName}}Attribute(string {{AutomaticInterfaceGenerator.NamespaceParameterName}} = default(string), string {{AutomaticInterfaceGenerator.InterfaceParameterName}} = default(string), bool {{AutomaticInterfaceGenerator.AsInternalParameterName}} = false) { }
